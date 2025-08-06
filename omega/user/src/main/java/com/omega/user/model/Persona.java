6318f81fc884e0e862c22a93d03418e28054ef48
ArmandoGabriel <armandogabrielvela@outlook.com>
2025-08-06 13:58:47 -0500

    Validaciones de los campos de persona para no repetir la identificación del cliente en el proyecto user

--- omega/user/src/main/java/com/omega/user/model/Persona.java
+++ omega/user/src/main/java/com/omega/user/model/Persona.java
@@ -1,6 +1,9 @@
 package com.omega.user.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -13,12 +16,26 @@ public class Persona {
 //    @GeneratedValue(strategy = GenerationType.IDENTITY)
 //    private Long personaId;
 
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(max = 50, message = "El nombre no puede tener más de 50 caracteres")
     private String nombre;
+
+    @NotBlank(message = "El género no puede estar vacío")
     private String genero;
+
+    @Min(value = 0, message = "La edad no puede ser negativa")
     private int edad;
+
+    @NotBlank(message = "La identificación no puede estar vacía")
+    @Column(unique = true, nullable = false)// Clave única en la base
     private String identificacion;
+
+    @NotBlank(message = "La dirección no puede estar vacía")
     private String direccion;
+
+    @NotBlank(message = "El teléfono no puede estar vacío")
     private String telefono;
+
 
     // Getters y setters
 
