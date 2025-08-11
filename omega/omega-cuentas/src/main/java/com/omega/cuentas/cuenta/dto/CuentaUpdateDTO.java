38ddc4113fb1c940cb072184c66e68bc40cf6e1e
ArmandoGabriel <armandogabrielvela@outlook.com>
2025-08-10 20:48:13 -0500

    Implementación de otros campos en update cuenta dto

--- omega/omega-cuentas/src/main/java/com/omega/cuentas/cuenta/dto/CuentaUpdateDTO.java
+++ omega/omega-cuentas/src/main/java/com/omega/cuentas/cuenta/dto/CuentaUpdateDTO.java
@@ -1,10 +1,15 @@
 package com.omega.cuentas.cuenta.dto;
 
 import com.omega.cuentas.cuenta.model.TipoCuenta;
+import java.math.BigDecimal;
 
 public class CuentaUpdateDTO {
     private TipoCuenta tipoCuenta;
     private Boolean estado;
+    
+    private String numeroCuenta;
+    private BigDecimal saldoInicial;
+    private BigDecimal saldoDisponible;
 
     // Getters y setters
 
@@ -24,4 +29,30 @@ public class CuentaUpdateDTO {
     public void setEstado(Boolean estado) {
         this.estado = estado;
     }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public BigDecimal getSaldoInicial() {
+        return saldoInicial;
+    }
+
+    public void setSaldoInicial(BigDecimal saldoInicial) {
+        this.saldoInicial = saldoInicial;
+    }
+
+    public BigDecimal getSaldoDisponible() {
+        return saldoDisponible;
+    }
+
+    public void setSaldoDisponible(BigDecimal saldoDisponible) {
+        this.saldoDisponible = saldoDisponible;
+    }
+    
+    
 }
